5c52d0b18c5b6c8a7b9975de0f888d5e5d690313
QuanbIn30 <821996940@qq.com>
2021-03-15 00:17:59 +0800

    use mybatis generator to get mapper and pojo

--- src/main/java/com/mhlevel/App.java
+++ src/main/java/com/mhlevel/App.java
@@ -1,13 +1,28 @@
 package com.mhlevel;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
+@EnableAutoConfiguration
+@RestController
+@RequestMapping("/App")
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world";
     }
 }
